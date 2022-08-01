85b446bfffd9319e383e90a7665e088c38648d09
Marittie <mario.castaldo1992@gmail.com>
2022-08-01 13:01:44 +0000

    Add function to determine the winner when 10 points are scored

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ const result = document.getElementById("result");
 let userScore = 0;
 let computerScore = 0;
 let tieScore = 0;
+const winningScore = 10;
 
 // Computer chooses a weapon randomly
 function generateComputerChoice() {
@@ -20,6 +21,7 @@ function generateComputerChoice() {
 function battle(userChoice) {
     let computerChoice = generateComputerChoice();
     displayResult(userChoice, computerChoice);
+    // Display user and Computer Choices
     document.querySelector('#userWeapon').innerHTML = userChoice;
     document.querySelector('#computerWeapon').innerHTML = computerChoice;
 }
@@ -33,6 +35,15 @@ const updateScore = () => {
     const computerScoreDisplay = document.getElementById("computerScore");
     computerScoreDisplay.innerHTML = computerScore;
   }
+
+  //Function to determine the Winner when scoring 10 points
+  function checkWinner() {
+    if (userScore == winningScore) {
+      result.innerHTML = 'You Win';
+} else if (computerScore == winningScore) {
+  result.innerHTML = 'Computer Win'
+}
+}
   
   // Show the result
   function displayResult(userChoice, computerChoice) {
@@ -40,90 +51,109 @@ const updateScore = () => {
       result.innerHTML = "same choice - It's a tie!";
       tieScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'scissors' && computerChoice === 'paper') {
       result.innerHTML = "scissors cuts paper - You Win!";
       userScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'paper' && computerChoice === 'scissors') {
       result.innerHTML = "scissors cuts paper - You Lose!";
       computerScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'paper' && computerChoice === 'rock') {
       result.innerHTML = "paper covers rock - You Win!";
       userScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice == 'rock' && computerChoice === 'paper') {
       result.innerHTML = "paper covers rock - You Lose!";
       computerScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'rock' && computerChoice === 'lizard') {
       result.innerHTML = "rock crushes lizard - You Win!";
       userScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'lizard' && computerChoice === 'rock') {
       result.innerHTML = "rock crushes lizard - You Lose!";
       computerScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'lizard' && computerChoice === 'spock') {
       result.innerHTML = "lizard poisons Spock - You Win!";
       userScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'spock' && computerChoice === 'lizard') {
       result.innerHTML = "lizard poisons Spock - You Lose!";
       computerScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'spock' && computerChoice === 'scissors') {
       result.innerHTML = "spock smashes scissors - You Win!";
       userScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'scissors' && computerChoice === 'spock') {
       result.innerHTML = "spock smashes scissors - You Lose!";
       computerScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'scissors' && computerChoice === 'lizard') {
       result.innerHTML = "scissors decapitate lizard - You Win!";
       userScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'lizard' && computerChoice === 'scissors') {
       result.innerHTML = "scissors decapitate lizard - You Lose!";
       computerScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'lizard' && computerChoice === 'paper') {
       result.innerHTML = "lizard eats paper - You Win!";
       userScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'paper' && computerChoice === 'lizard') {
       result.innerHTML = "lizard eats paper - You Lose!";
       computerScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'paper' && computerChoice === 'spock') {
       result.innerHTML = "paper disproves Spock - You Win!";
       userScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'spock' && computerChoice === 'paper') {
       result.innerHTML = "paper disproves Spock - You Lose!";
       computerScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'spock' && computerChoice === 'rock') {
       result.innerHTML = "Spock vaporizes rock - You Win!";
       userScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'rock' && computerChoice === 'spock') {
       result.innerHTML = "Spock vaporizes rock - You Lose!";
       computerScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'rock' && computerChoice === 'scissors') {
       result.innerHTML = "rock crushes scissors - You Win!";
       userScore++;
       updateScore();
+      checkWinner();
     } else if (userChoice === 'scissors' && computerChoice === 'rock') {
       result.innerHTML = "rock crushes scissors - You Lose!";
       computerScore++;
       updateScore();
-    }
+      checkWinner();
+    } 
   }
-
-
 
 // EventListeners for all buttons
 rock.addEventListener("click", function () {
